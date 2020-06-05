a3b66fa190836f6c96a62e82f8abe3ea0ab05df8
fmnb0516 <fmnb0516@mnb.bz>
2020-06-05 18:03:23 +0900

    update.

--- modules/ja/init.js
+++ modules/ja/init.js
@@ -6,7 +6,7 @@ module.exports = (context) => {
         const camp_data = eval("(() => { return " + res1.substring(res1.indexOf("var camp_data = ") + "var camp_data = ".length, res1.indexOf("var topic_data = ")) + "})();");
         const topic_data = eval("(() => { return " + res1.substring(res1.indexOf("var topic_data = ") + "var topic_data = ".length, res1.indexOf("</script>", res1.indexOf("var topic_data = "))) + ";})();");
         
-        const res2 = await context.common.requestWithCache("https://gamepress.gg/sites/default/files/aggregatedjson/E7CampsiteSimulator.json");
+        const res2 = await context.common.requestWithCache("https://gamepress.gg/sites/default/files/aggregatedjson/E7CampsiteSimulator.json", true);
         const hero_data = JSON.parse(res2);
 
         const mapping = await context.common.readJson("./modules/ja/cache/hero_name_mapping.json", {});
