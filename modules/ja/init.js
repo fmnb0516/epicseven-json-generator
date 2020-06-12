34ce2cf7c1b9f105fbfbcf5a2c9d92b909b9cbfd
fmnb0516 <fmnb0516@mnb.bz>
2020-06-12 11:45:09 +0900

    update.

--- modules/ja/init.js
+++ modules/ja/init.js
@@ -2,7 +2,7 @@ module.exports = (context) => {
 
     const initalized = async() => {
 
-        const res1 = await context.common.requestWithCache("https://gamepress.gg/epicseven/camping-simulator");
+        const res1 = await context.common.requestWithCache("https://gamepress.gg/epicseven/camping-simulator", true);
         const camp_data = eval("(() => { return " + res1.substring(res1.indexOf("var camp_data = ") + "var camp_data = ".length, res1.indexOf("var topic_data = ")) + "})();");
         const topic_data = eval("(() => { return " + res1.substring(res1.indexOf("var topic_data = ") + "var topic_data = ".length, res1.indexOf("</script>", res1.indexOf("var topic_data = "))) + ";})();");
         
