e77d9f7d29a968dc5e9122b9dc5af1f42b2ff079
fmnb0516 <fmnb0516@mnb.bz>
2020-06-12 10:37:15 +0900

    update.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const future = (async (modules) => {
-    const DataBuilder = require("./modules/data-builder.js");
-    const DataGenerator = require("./modules/data-generator.js");
 
-    const lang = process.argv[2] !== undefined && process.argv[2] !== null ? process.argv[2] : "ja";
+const program = require("commander");
+const modules = (() => {
+    return {
+        request : require('request-promise'),
+        cheerio : require('cheerio'),
+        crypto : require('crypto'),
+        fs : require('fs').promises,
+        util : require('util'),
+        express : require('express')
+    };
+})();
 
-    const generateCachePath = (url) => {
-        const md5 = modules.crypto.createHash('md5')
-        const hash = md5.update(url, 'binary').digest('hex');
-        return "./modules/" + lang + "/cache/" + hash + ".html";
+const common = (() => {
+
+    const convertIntNum = (num, def) => {
+        const n = parseInt(num);
+        return Number.isNaN(n) || n <= 0 ? def : n;
     };
 
     const readFileNoErr = async (path, enc) => {
@@ -22,6 +30,79 @@ const future = (async (modules) => {
         const def2 = def === undefined ? null : def;
         const json = await readFileNoErr(path, "utf8");
         return json !== null ? JSON.parse(json) : def2;
+    };
+
+    const trraversalJsonFiles = async (path, excluede, handler) => {
+
+        const files = await modules.fs.readdir(path);
+
+        for (let i = 0; i < files.length; i++) {
+            const fname = files[i];
+            if(excluede.indexOf(fname) !== -1) {
+                continue;
+            }
+
+            const json = await readJson(path + fname);
+            await handler(fname, json);   
+        }
+    };
+
+    return {
+        convertIntNum : convertIntNum,
+        readJson : readJson,
+        trraversalJsonFiles : trraversalJsonFiles
+    };
+
+})();
+
+
+
+program.version('0.0.1', '-v, --version');
+
+program.command('mock [port]')
+    .description('start mock web server, deafult port is [3000]')
+    .action((port) => {
+        const portNum = common.convertIntNum(port, 3000);
+        modules.express()
+            .use(modules.express.static('./docs'))
+            .listen(process.env.PORT || 3000);
+        console.log("listen: " + portNum);
+        console.log("access to http://localhost:" + portNum);
+    });
+
+program.command('checkcamping')
+    .description('list up hero name of no camping data.')
+    .action(() => {
+
+        common.trraversalJsonFiles("docs/ja/hero/", ["heros.json"], (file, json) => {
+            if(json.extention === undefined || json.extention.camping === undefined) {
+                console.log(file);
+            }
+        });
+    });
+
+program.command('cachename <url>')
+    .description('generate url => cachename')
+    .action((url) => {
+        const md5 = modules.crypto.createHash('md5')
+        const hash = md5.update(url, 'binary').digest('hex');
+        console.log(url + " => " + hash);
+    });
+
+program.parse(process.argv);
+
+/*
+
+const future = (async (modules) => {
+    const DataBuilder = require("./modules/data-builder.js");
+    const DataGenerator = require("./modules/data-generator.js");
+
+    const lang = process.argv[3] !== undefined && process.argv[3] !== null ? process.argv[3] : "ja";
+
+    const generateCachePath = (url) => {
+        const md5 = modules.crypto.createHash('md5')
+        const hash = md5.update(url, 'binary').digest('hex');
+        return "./modules/" + lang + "/cache/" + hash + ".html";
     };
     
     const requestWithCache = async (url, force) => {
@@ -97,9 +178,9 @@ const future = (async (modules) => {
         },
 
         generate : async () => {
-            const gen = new DataGenerator.Generator();
-            await require("./modules/" + lang + "/index.js")(gen.toBuilder(), "./docs/"+lang, common);
-            await gen.generate("./docs/"+lang, common);
+            //const gen = new DataGenerator.Generator();
+            //await require("./modules/" + lang + "/index.js")(gen.toBuilder(), "./docs/"+lang, common);
+            //await gen.generate("./docs/"+lang, common);
             return this;
         }
     };
@@ -116,3 +197,4 @@ future.then(app => app.generate().then(() => app)).then(app => {
     app.mockserver();
     console.log("listen: 3000");
 });
+*/
